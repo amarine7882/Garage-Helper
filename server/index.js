f7185120201a3ac1e9737931f89ed6226c001eca
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-13 17:01:46 -0700

    Added timestamp logging for development

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
+const moment = require('moment');
 const server = require('./server.js');
 
 const PORT = 3000;
 
-server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`listening on port ${PORT} @ ${moment().format('h:mm:ss')}`));
