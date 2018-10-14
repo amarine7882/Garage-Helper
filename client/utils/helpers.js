91de8c70ddcad88dc5509ac1b52d64c9c3bf2abf
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-14 15:50:03 -0700

    Added helpers module

---
+++ client/utils/helpers.js
@@ -0,0 +1,15 @@
+const generateYears = () => {
+  let years = [];
+
+  // Adding plus one to year to account for next model year cars
+  const start = 1900;
+  const end = new Date().getFullYear() + 1;
+
+  for (let year = start; year <= end; year += 1) {
+    years = [...years, year];
+  }
+
+  return years;
+};
+
+export default generateYears;
