0524409029e1c93b9ee9695183f23e81ec1d9e82
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-13 17:02:39 -0700

    Page gets user name on load and renders Form creator

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CarContainer from './CarContainer';
+import NewCarForm from './NewCarForm';
 
-const App = () => <div>Hello World</div>;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      userId: '',
+      isCreating: false,
+    };
+
+    this.displayNewCarForm = this.displayNewCarForm.bind(this);
+  }
+
+  componentDidMount() {
+    const userId = prompt('User Name:');
+
+    this.setState({ userId });
+  }
+
+  displayNewCarForm() {
+    this.setState({ isCreating: true });
+  }
+
+  render() {
+    const { isCreating } = this.state;
+
+    return (
+      <div>
+        <button type="button" onClick={this.displayNewCarForm}>
+          Create New Car
+        </button>
+        <NewCarForm isCreating={isCreating} />
+      </div>
+    );
+  }
+}
