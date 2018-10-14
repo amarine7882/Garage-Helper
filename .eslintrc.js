f398ac9ca8109dccab4239a37736d6a63a659c04
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-14 12:43:42 -0700

    configured eslint/prettier airbnb integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
   env: {
     browser: true,
-    node: true,
-    jest: true,
+    node: true
   },
   rules: {
     'no-console': 0,
-  },
+    'prettier/prettier': ['error']
+  }
 };
