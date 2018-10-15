60643a42cf717f14bf51ae1e8f78e07d41128a38
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-14 21:49:22 -0700

    Selected car renders to page

--- client/src/components/Car.jsx
+++ client/src/components/Car.jsx
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 export default class Car extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      carData: undefined
+    };
+
+    this.getCarData = this.getCarData.bind(this);
+  }
+
+  componentDidUpdate(prevState, prevProps) {
+    const { displayedCar } = this.props;
+
+    if (prevProps.displayedCar !== displayedCar) {
+      this.getCarData();
+    }
+  }
+
+  getCarData() {
+    const { displayedCar } = this.props;
+
+    if (!displayedCar) return;
+
+    axios
+      .get(`api/cars/${displayedCar}`)
+      .then(({ data }) => this.setState({ carData: data }))
+      .catch(err => console.log(err));
   }
 
   render() {
+    const { carData } = this.state;
     const { displayedCar } = this.props;
 
-    if (!displayedCar) return null;
+    if (!displayedCar || !carData) return null;
 
-    return <div>Car</div>;
+    const { carName, make, model, modelYear } = carData;
+
+    return (
+      <div>
+        <div>{carName}</div>
+        <div>{make}</div>
+        <div>{model}</div>
+        <div>{modelYear}</div>
+      </div>
+    );
   }
 }
 
-Car.defaultProps = {
-  displayedCar: undefined
-};
-
 Car.propTypes = {
-  displayedCar: PropTypes.string
+  displayedCar: PropTypes.string.isRequired
 };
