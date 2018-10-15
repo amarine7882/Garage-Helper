4b86070ca98c055bd345cd66bbad914453d5b63f
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-15 11:24:46 -0700

    changed folder name to helpes

---
+++ client/helpers/helpers.js
@@ -0,0 +1,20 @@
+export const generateModelYears = () => {
+  let years = [];
+
+  // Adding plus one to year to account for next model year cars
+  const start = 1900;
+  const end = new Date().getFullYear() + 1;
+
+  for (let year = start; year <= end; year += 1) {
+    years = [...years, year];
+  }
+
+  return years;
+};
+
+export const makeCarListValue = car => {
+  if (car.carName) {
+    return car.carName;
+  }
+  return `${car.modelYear} ${car.make} ${car.model}`;
+};
