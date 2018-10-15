c52b3b18481ffc500dab993090177d95823d9eb4
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-14 23:36:22 -0700

    Added service item endpoints

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,20 @@ const controller = require('./controllers');
 
 const router = express.Router();
 
+// <----------- Car endpoints ---------------->
+
 router.get('/cars/:carID', controller.cars.get);
 
 router.post('/cars', express.json(), controller.cars.post);
 
+// < ---------------- User Endpoints ------------- >
+
 router.get('/users/:userID/cars', controller.users.get);
+
+// <----------------- Service Item endpoints ---------->
+
+router.get('/cars/:carID/serviceItems', controller.serviceItems.get);
+
+router.post('/cars/:carID/serviceItems', express.json(), controller.serviceItems.post);
 
 module.exports = router;
