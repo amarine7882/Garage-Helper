452cf91b0765c5cbdddf53484c860567b423bd47
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-19 14:24:44 -0700

    added async/await plugins and semantic ui

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/public/dist');
+const BUILD_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR,
+    path: BUILD_DIR
   },
   module: {
     rules: [
@@ -17,12 +17,12 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/react', '@babel/preset-env'],
-          plugins: ['babel-plugin-styled-components'],
-        },
-      },
-    ],
+          plugins: ['babel-plugin-styled-components', '@babel/plugin-transform-async-to-generator']
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: ['.js', '.json', '.jsx', '.css'],
-  },
+    extensions: ['.js', '.json', '.jsx', '.css']
+  }
 };
