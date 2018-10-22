35fe7502e4d8d102d8cdce3b417164e320134f2f
Andrew Marine <andrewmarine7882@gmail.com>
2018-10-21 18:40:48 -0700

    Added css loading in webpack and semantic ui css files

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,25 @@ module.exports = {
           presets: ['@babel/react', '@babel/preset-env'],
           plugins: ['babel-plugin-styled-components', '@babel/plugin-transform-async-to-generator']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      },
+      {
+        test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
+        loader: require.resolve('file-loader'),
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
       }
     ]
   },
