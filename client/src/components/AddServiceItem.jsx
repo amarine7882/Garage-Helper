006888b0377a91d1e4810ed1ff01941fa4e68a13
Andrew Marine <andrewmarine7882@gmail.com>
2018-11-03 01:58:17 -0700

    Moved to ant controlled forms, added validation

--- client/src/components/AddServiceItem.jsx
+++ client/src/components/AddServiceItem.jsx
@@ -1,91 +1,95 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Form, Input, InputNumber, Button } from 'antd';
 
 import { postServiceItem } from '../../network/serviceItemRequests';
 
-export default class AddServiceItem extends Component {
+class AddServiceItemTemplate extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      serviceName: '',
-      serviceIntervalMonths: 0,
-      serviceIntervalMiles: 0
-    };
-
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleFormInput = this.handleFormInput.bind(this);
+    this.validationSync = this.validationSync.bind(this);
+    this.serviceIntervalValidator = this.serviceIntervalValidator.bind(this);
   }
 
-  handleFormInput({ target }) {
-    const { name, value } = target;
-    this.setState({ [name]: value });
-  }
-
-  async handleSubmit(e) {
+  handleSubmit(e) {
     e.preventDefault();
-    const { displayedCar, userID, toggleAddServiceItem } = this.props;
-    const { serviceName, serviceIntervalMonths, serviceIntervalMiles } = this.state;
+    const { displayedCar, userID, toggleAddServiceItem, form } = this.props;
 
-    if (serviceName.length < 1 || (serviceIntervalMiles < 1 && serviceIntervalMonths < 1)) return;
+    form.validateFields(async (err, values) => {
+      if (!err) {
+        await postServiceItem(userID, displayedCar, values);
+        toggleAddServiceItem();
+      }
+    });
+  }
 
-    const payload = {
-      serviceIntervalMonths,
-      serviceIntervalMiles,
-      serviceName
-    };
+  serviceIntervalValidator(rule, value, callback) {
+    const { form } = this.props;
+    const mileIntervalValue = form.getFieldValue('serviceIntervalMiles');
+    const monthIntervalValue = form.getFieldValue('serviceIntervalMonths');
 
-    await postServiceItem(userID, displayedCar, payload);
-    toggleAddServiceItem();
+    if (!mileIntervalValue && !monthIntervalValue) {
+      callback('You must enter at least one valid service interval value');
+    } else {
+      callback();
+    }
+  }
+
+  validationSync() {
+    const { form } = this.props;
+    form.validateFields(['serviceIntervalMonths', 'serviceIntervalMiles'], { force: true });
   }
 
   render() {
-    const { serviceName, serviceIntervalMonths, serviceIntervalMiles } = this.state;
-
-    const isEnabled =
-      serviceName.length > 0 && (serviceIntervalMonths > 0 || serviceIntervalMiles > 0);
-
+    const { form } = this.props;
+    const { getFieldDecorator } = form;
     return (
       <div>
-        <form>
-          <label htmlFor="serviceName">
-            Service Name:
-            <input
-              name="serviceName"
-              type="text"
-              value={serviceName}
-              onChange={this.handleFormInput}
-            />
-          </label>
-          <label htmlFor="serviceIntervalMonths">
-            Service Interval by Months:
-            <input
-              name="serviceIntervalMonths"
-              type="number"
-              min="0"
-              value={serviceIntervalMonths}
-              onChange={this.handleFormInput}
-            />
-          </label>
-          <label htmlFor="serviceIntervalMiles">
-            Service Interval by Miles:
-            <input
-              name="serviceIntervalMiles"
-              type="number"
-              min="0"
-              value={serviceIntervalMiles}
-              onChange={this.handleFormInput}
-            />
-          </label>
-          <input type="submit" disabled={!isEnabled} onClick={this.handleSubmit} />
-        </form>
+        <Form onSubmit={this.handleSubmit} style={{ width: 500 }}>
+          <Form.Item label="Service Name" colon={false}>
+            {getFieldDecorator('serviceName', {
+              rules: [{ required: true, message: 'Please input a name for the service' }]
+            })(<Input placeholder="Service Name" />)}
+          </Form.Item>
+          <Form.Item label="Service Interval In Months" colon={false}>
+            {getFieldDecorator('serviceIntervalMonths', {
+              rules: [{ validator: this.serviceIntervalValidator }]
+            })(
+              <InputNumber
+                placeholder="Months"
+                min={0}
+                style={{ width: 200 }}
+                onChange={() => setTimeout(this.validationSync, 10)}
+              />
+            )}
+          </Form.Item>
+          <Form.Item label="Service Interval In Miles" colon={false}>
+            {getFieldDecorator('serviceIntervalMiles', {
+              rules: [{ validator: this.serviceIntervalValidator }]
+            })(
+              <InputNumber
+                placeholder="Miles"
+                min={0}
+                style={{ width: 200 }}
+                onChange={() => setTimeout(this.validationSync, 10)}
+              />
+            )}
+          </Form.Item>
+          <Button htmlType="submit">Submit</Button>
+        </Form>
       </div>
     );
   }
 }
 
-AddServiceItem.propTypes = {
+const AddServiceItem = Form.create()(AddServiceItemTemplate);
+
+export default AddServiceItem;
+
+AddServiceItemTemplate.propTypes = {
   displayedCar: PropTypes.string.isRequired,
   userID: PropTypes.string.isRequired,
-  toggleAddServiceItem: PropTypes.func.isRequired
+  toggleAddServiceItem: PropTypes.func.isRequired,
+  form: PropTypes.instanceOf(Object).isRequired
 };
