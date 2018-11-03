70aec94f8432835ec185bc97a68a35ffde10dbe3
Andrew Marine <andrewmarine7882@gmail.com>
2018-11-03 01:58:40 -0700

    Added edge case handling

--- client/helpers/helpers.js
+++ client/helpers/helpers.js
@@ -21,33 +21,40 @@ export const makeCarListValue = car => {
   return `${car.modelYear} ${car.make} ${car.model}`;
 };
 
-export const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+export const numberWithCommas = x => {
+  if (x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+  return undefined;
+};
 
 export const displayIntervalsIfPresent = (serviceIntervalMonths, serviceIntervalMiles) => {
   let intervalString = '';
 
   if (serviceIntervalMonths > 0 && serviceIntervalMiles > 0) {
-    intervalString = `Due every ${serviceIntervalMonths} months or ${serviceIntervalMiles} miles`;
+    intervalString = `Due every ${serviceIntervalMonths} months or ${numberWithCommas(
+      serviceIntervalMiles
+    )} miles`;
   } else if (serviceIntervalMonths > 0) {
     intervalString = `Due every ${serviceIntervalMonths} months`;
   } else {
-    intervalString = `Due every ${serviceIntervalMiles} miles`;
+    intervalString = `Due every ${numberWithCommas(serviceIntervalMiles)} miles`;
   }
 
   return intervalString;
 };
 
-export const displayNextDueIfPresent = (nextDueDate, nextDueMiles) => {
+export const displayNextDueIfPresent = (nextDueDate, nextDueMiles, serviceIntervalMonths) => {
   let nextDueString = '';
 
-  if (nextDueMiles && nextDueDate) {
+  if (nextDueMiles && serviceIntervalMonths) {
     nextDueString = `Next Due on ${moment(nextDueDate).calendar()} or at ${numberWithCommas(
       nextDueMiles
     )} miles`;
   } else if (!nextDueDate && !nextDueMiles) {
     nextDueString = '';
-  } else if (!nextDueDate) {
-    nextDueString = `Next Due at ${nextDueMiles} miles`;
+  } else if (!serviceIntervalMonths) {
+    nextDueString = `Next Due at ${numberWithCommas(nextDueMiles)} miles`;
   } else if (!nextDueMiles) {
     nextDueString = `Next Due on ${moment(nextDueDate).calendar()}`;
   }
