64115d655d71c399c98b6e7c7a969f9f1386bde4
Andrew Marine <andrewmarine7882@gmail.com>
2018-11-12 19:40:40 -0800

    added production webpack scripts and config

---
+++ serverProd.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+
+const SRC_DIR = path.join(__dirname, '/server');
+const DIST_DIR = path.join(__dirname, '/production/server/dist');
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .forEach(mod => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  target: 'node',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.css']
+  },
+  externals: nodeModules
+};
